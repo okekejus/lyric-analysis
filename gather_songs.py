90771ffe8992167984f19e53bc9662dee6f6b135
Justin Okeke, MPH <justinokeke@hotmail.com>
2021-10-29 12:12:58 -0400

    Update gather_songs.py

--- gather_songs.py
+++ gather_songs.py
@@ -12,58 +12,50 @@ Can download as df/csv in R, but comes out rather messy.
 
 import lyricsgenius as genius
 import pandas as pd
+import dask
+from dask import delayed
+import os, json
+from pandas.io.json import json_normalize
 
+api_token = "token"
 
-# set api up, create conditions for downloads (excluding remixes and live), 
-# skipping songs the artist is featured on as well.
-genius = genius.Genius('xgHL7_q6VGQaKz5Id0HhDCyIncpineDkZX8ovvORsO2Wum_c3-hUFr-7pK56ukyH', 
+artists = pd.read_csv("Artist_List.csv", encoding='latin-1', index_col=0)
+artist_name = artists['artist_name']
+
+genius = genius.Genius(api_token,
                    skip_non_songs=True, 
                    excluded_terms=["(Remix)", "(Live)"], 
-                   remove_section_headers=True)
+                   remove_section_headers=True, 
+                   retries=1,
+                   timeout=10,
+                )
 
-# Testing the api to see if it works 
-artist = genius.search_artist("Drake", max_songs=5)
+genius.verbose = False
 
-del artist
+@delayed
+def get_lyrics(artist):
+    try:
+        lyrics = genius.search_artist(artist, max_songs = 150)
+        lyrics.save_lyrics()
+    except Timeout:
+        pass
+
+delayed_results = []
+
+for artist in artist_name:
+    observed = get_lyrics(artist)
+    delayed_results.append(observed)
+
+results = dask.compute(*delayed_results)
+
+json_files = [file for file in os.listdir() if file.endswith('.json')]
 
 
-# Read in the list of artists made before
-main_list = pd.read_csv("data/Artist_List.csv")
+li = []
+for file in json_files:
+    with open(file) as json_data:
+        data = json.load(json_data)
+        df = pd.DataFrame(data['songs'])
+        li.append(df)
 
-# correcting issues with the names
-artists = list(main_list['artist_name'])
-artists[4] = 'Beyoncé'
-artists[10] = "Céline Dion"
-artists[19] = "Guns N' Roses"
-
-main_list['artist_name'] = artists
- 
-
-# downloading lyrics to .json file
-for artist in artists: 
-    try: 
-        lyrics = genius.search_artist(artist, max_songs = 150) 
-        lyrics.save_lyrics() 
-    except: 
-        print(["API timeout at ", artist]) # the API times out, so when it does, we want it to start over with the same artist it was on - preventing losses. 
-        lyrics = genius.search_artist(artist, max_songs = 150) 
-        lyrics.save_lyrics() 
-        
-        
- # EDIT: Making up for missing songs (songs excluded from the downloads due to the filtered keywords)
-artists = ["Doja Cat", "Sade", "Blake Shelton", "Bruce Springsteen", "The Beatles"]
-
-for artist in artists: 
-    try: 
-        lyrics = genius.search_artist(artist, max_songs = 150) 
-        lyrics.save_lyrics() 
-    except: 
-        print(["API timeout at ", artist]) 
-        lyrics = genius.search_artist(artist, max_songs = 150) 
-        lyrics.save_lyrics() 
- 
-
- # Resulting download is a .json file. I prefer to edit these using R.     
-        
-        
-        
+frame = pd.concat(li, axis=0, ignore_index=True)
