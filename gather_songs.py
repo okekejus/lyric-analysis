1c842cf35be8fd5da19649d4f71fc18d4cb28c1c
Justin Okeke, MPH <justinokeke@hotmail.com>
2022-04-18 16:30:16 -0400

    Update gather_songs.py

--- gather_songs.py
+++ gather_songs.py
@@ -35,27 +35,48 @@ genius.verbose = False
 @delayed
 def get_lyrics(artist):
     try:
-        lyrics = genius.search_artist(artist, max_songs = 150)
+        lyrics = genius.search_artist(artist, max_songs = 200)
         lyrics.save_lyrics()
+    except HTTPError as e:
+        print(e.errno)    # status code
+        print(e.args[0])  # status code
+        print(e.args[1])  # error message
+        
+        if e.args[1].startswith('5'):
+            time.sleep(5)
+            lyrics = genius.search_artist(artist, max_songs = 200)
+            lyrics.save_lyrics()
     except Timeout:
         pass
 
 delayed_results = []
 
-for artist in artist_name:
+for artist in artists:
     observed = get_lyrics(artist)
     delayed_results.append(observed)
 
 results = dask.compute(*delayed_results)
 
+# turning them all into one data frame 
 json_files = [file for file in os.listdir() if file.endswith('.json')]
 
 
 li = []
+
+
 for file in json_files:
     with open(file) as json_data:
         data = json.load(json_data)
         df = pd.DataFrame(data['songs'])
         li.append(df)
+        
+songs = pd.concat(li, axis=0, ignore_index=True)
+del data, df, file, json_data, json_files, li
 
-frame = pd.concat(li, axis=0, ignore_index=True)
+
+# extracting just the songs
+songs = songs[["artist","title", "lyrics"]]
+
+  
+
+
